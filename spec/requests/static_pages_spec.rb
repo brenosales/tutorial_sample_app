dc42030645d9b4dafe3bae8ad8a289dfe050203f
vagrant <vagrant@precise64.(none)>
2013-06-06 11:46:04 -0300

    Refactoring static-pages-title

--- spec/requests/static_pages_spec.rb
+++ spec/requests/static_pages_spec.rb
@@ -1,18 +1,22 @@
 require 'spec_helper'
 
 describe "Static Pages" do
-  let(:base_title) { "Sample App" }
 
   describe "Home page" do
 
     it "should have the content 'Sample App'" do
       visit "/static_pages/home"
-      expect(page).to have_content("#{base_title}")
+      expect(page).to have_content("Sample App")
     end
 
-    it "should have the title 'Home'" do
+    it "should have the base title" do
       visit "/static_pages/home"
-      expect(page).to have_title("#{base_title} | Home")
+      expect(page).to have_title("Sample App")
+    end
+
+    it "should not have a custom page title" do
+      visit "/static_pages/home"
+      expect(page).not_to have_title("| Home")
     end
   end
 
@@ -24,7 +28,7 @@ describe "Static Pages" do
 
     it "should have the title 'Help'" do
       visit "/static_pages/help"
-      expect(page).to have_title("#{base_title} | Help")
+      expect(page).to have_title("| Help")
     end
   end
 
@@ -36,7 +40,7 @@ describe "Static Pages" do
 
     it "should have the title 'About Us'" do
       visit "/static_pages/about"
-      expect(page).to have_title("#{base_title} | About Us")
+      expect(page).to have_title("| About Us")
     end
   end
 
@@ -48,7 +52,7 @@ describe "Static Pages" do
 
     it "should have the title 'Contact Us'" do
       visit "/static_pages/contact"
-      expect(page).to have_title("#{base_title} | Contact Us")
+      expect(page).to have_title("| Contact Us")
     end
   end
 end
