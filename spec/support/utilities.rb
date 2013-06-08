0b5a52bd88ab01e00c2da141c6ee5214f01090c0
brenosales <brenosales@gmail.com>
2013-06-08 19:22:31 -0300

    Finishing layout and routes

---
+++ spec/support/utilities.rb
@@ -0,0 +1,9 @@
+# Return the full title on aper-page basis.
+  def full_title(page_title)
+    base_title = "Sample App"
+    if page_title.empty?
+      base_title
+    else
+      "#{base_title} | #{page_title}"
+    end
+  end
